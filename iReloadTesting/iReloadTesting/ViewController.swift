836171c06c77768edd3a294d2c26a3535b6f2b65
黃宇新 <astroboy03@cathaylife.com.tw>
2020-11-24 15:12:17 +0800

    更新測試方式

--- iReloadTesting/iReloadTesting/ViewController.swift
+++ iReloadTesting/iReloadTesting/ViewController.swift
@@ -9,7 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private var datas = [TCellViewModel]()
+    private let dataQueue = DispatchQueue(label: "_showDataQueue")
+    
+    private var aDatas = [TCellViewModel]() {
+        didSet {
+            self.refreshView(datas: self.aDatas)
+        }
+    }
+    
+    private var bDatas = [TCellViewModel]() {
+        didSet {
+            self.refreshView(datas: self.bDatas)
+        }
+    }
+    
+    private var cDatas = [TCellViewModel]() {
+        didSet {
+            self.refreshView(datas: self.cDatas)
+        }
+    }
+    
+    private var datas = [TCellViewModel]() {
+        didSet {
+            dataQueue.async {
+                let semaphore = DispatchSemaphore(value: 0)
+                DispatchQueue.main.async {
+                    // 無動畫 - 1
+//                    UIView.performWithoutAnimation {
+//                        self.dataView?.reloadSections(IndexSet(integer: 0))
+//                        semaphore.signal()
+//                    }
+
+                    // 無動畫 - 2
+                    CATransaction.begin()
+                    CATransaction.setDisableActions(true)
+                    self.dataView?.reloadSections(IndexSet(integer: 0))
+                    semaphore.signal()
+                    CATransaction.commit()
+                    
+//                    // 2 動畫 - 1
+//                    UIView.animate(withDuration: 0.3) {
+//                        self.dataView?.reloadSections(IndexSet(integer: 0))
+//                    } completion: { (_) in
+//                        semaphore.signal()
+//                    }
+                }
+                semaphore.wait()
+            }
+        }
+    }
     
     @IBOutlet private var dataView: UICollectionView?
     
@@ -24,21 +72,59 @@ class ViewController: UIViewController {
     }
     
     @IBAction private func doReload(_ sender: UIButton) {
-        let queue = DispatchQueue(label: "TReloading")
-        for degree in 1...10 {
-            let IntTime = Int.random(in: 1...5)
-            let afterTime = Double(IntTime) / 10
-            print("\(degree) after >>>> \(afterTime)")
-            queue.asyncAfter(deadline: .now() + afterTime) {
-                print("in >>>> \(degree)")
-                self.datas.removeAll()
-                for idx in 1...Int.random(in: 10...20) {
-                    self.datas.append(TCellViewModel(text: "(\(degree))index: \(idx)"))
-                }
-                DispatchQueue.main.async {
-                    self.dataView?.reloadData()
+        
+        DispatchQueue.global().async {
+            for degree in 1...10 {
+                let IntTime = Int.random(in: 1...5)
+                let afterTime = Double(IntTime) / 10
+                print("\(degree) a after >>>> \(afterTime)")
+                DispatchQueue.global().async {
+                    print("ain >>>> \(degree)")
+                    var inDatas = [TCellViewModel]()
+                    for idx in 1...Int.random(in: 10...20) {
+                        inDatas.append(TCellViewModel(text: "a (\(degree))index: \(idx)"))
+                    }
+                    self.aDatas = inDatas
                 }
             }
+        }
+        
+        DispatchQueue.global().async {
+            for degree in 1...10 {
+                let IntTime = Int.random(in: 1...5)
+                let afterTime = Double(IntTime) / 10
+                print("\(degree) b after >>>> \(afterTime)")
+                DispatchQueue.global().async {
+                    print("bin >>>> \(degree)")
+                    var inDatas = [TCellViewModel]()
+                    for idx in 1...Int.random(in: 10...20) {
+                        inDatas.append(TCellViewModel(text: "b (\(degree))index: \(idx)"))
+                    }
+                    self.bDatas = inDatas
+                }
+            }
+        }
+        
+        DispatchQueue.global().async {
+            for degree in 1...10 {
+                let IntTime = Int.random(in: 1...5)
+                let afterTime = Double(IntTime) / 10
+                print("\(degree) c after >>>> \(afterTime)")
+                DispatchQueue.global().async {
+                    print("cin >>>> \(degree)")
+                    var inDatas = [TCellViewModel]()
+                    for idx in 1...Int.random(in: 10...20) {
+                        inDatas.append(TCellViewModel(text: "c (\(degree))index: \(idx)"))
+                    }
+                    self.cDatas = inDatas
+                }
+            }
+        }
+    }
+    
+    private final func refreshView(datas: [TCellViewModel]) {
+        self.dataQueue.async {
+            self.datas = datas
         }
     }
 }
